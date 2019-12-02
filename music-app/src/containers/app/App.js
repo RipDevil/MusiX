a6ef8b4db9b03afad6796ce4317213e0dabaf01f
Mark Naberukhin <mna@startext.ru>
2019-12-02 16:44:54 +0700

    Minor changes: - added some files - changed files structure - added new packages: bootstrap* & font-awesome
    * - might be odd

---
+++ music-app/src/containers/app/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+// import { Button, Row, Col } from "antd"
+// import "antd/dist/antd.css";
+
+// function App() {
+//   return (
+//     <>
+//       <Row align="middle" type="flex" justify="center">
+//         <Col>
+//             <Row style={{marginTop:"50%"}} type="flex" justify="center">
+//               <h1>MA</h1>
+//             </Row>
+//             <Row type="flex" justify="center">
+//               <Button onClick={() => {
+//                 alert("Clicked!");
+//               }} type="default">BUTTON</Button>
+//             </Row>
+//         </Col>
+//       </Row>
+//     </>
+//   );
+// }
+
+function App() {
+  return (
+    <h1>MusicApp</h1>
+  );
+}
+
+export default App;
--- music-app/src/index.js
+++ music-app/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import App from './containers/app/App';
+
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.css'; // TODO: maybe isn't needed because there is Ant
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
     <App />,
