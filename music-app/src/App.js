1c83c9a5dd9755f6063025c9a9516180e7da1895
Mark Naberukhin <mna@startext.ru>
2019-12-02 15:48:50 +0700

    Packages: - antd - axios - react-animations
    and some changes to test those packages

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
 
+import { Button, Row, Col } from "antd"
+import "antd/dist/antd.css";
+
 function App() {
   return (
-    <h1 style={{paddingLeft: "40px"}}>
-      Hello cruel world!
-    </h1>
+    <>
+      <Row align="middle" type="flex" justify="center">
+        <Col>
+            <Row style={{marginTop:"50%"}} type="flex" justify="center">
+              <h1>MA</h1>
+            </Row>
+            <Row type="flex" justify="center">
+              <Button onClick={() => {
+                alert("Clicked!");
+              }} type="default">BUTTON</Button>  
+            </Row>     
+        </Col>          
+      </Row>
+    </>
+    
   );
 }
 
