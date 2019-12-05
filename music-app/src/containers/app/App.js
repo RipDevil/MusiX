fc971260c3b66b099eefa7c48b074fc011e1a2f7
RipDevil <tiberi66@yandex.ru>
2019-12-05 17:34:33 +0700

    WIP: loading page

--- music-app/src/containers/app/App.js
+++ music-app/src/containers/app/App.js
@@ -1,30 +1,15 @@
 import React from 'react';
+// import Loading from '../loading/Loading'
+import { Row, Col } from "antd"
 
-// import { Button, Row, Col } from "antd"
-// import "antd/dist/antd.css";
-
-// function App() {
-//   return (
-//     <>
-//       <Row align="middle" type="flex" justify="center">
-//         <Col>
-//             <Row style={{marginTop:"50%"}} type="flex" justify="center">
-//               <h1>MA</h1>
-//             </Row>
-//             <Row type="flex" justify="center">
-//               <Button onClick={() => {
-//                 alert("Clicked!");
-//               }} type="default">BUTTON</Button>
-//             </Row>
-//         </Col>
-//       </Row>
-//     </>
-//   );
-// }
 
 function App() {
   return (
-    <h1>MusicApp</h1>
+      <Row type="flex" justify="center" align="middle">
+        <Col span={4}>
+          <i class="fa fa-circle-o-notch fa-spin fa-5x fa-fw"></i>
+        </Col>
+      </Row>
   );
 }
 
--- music-app/src/index.js
+++ music-app/src/index.js
@@ -6,6 +6,7 @@ import App from './containers/app/App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css'; // TODO: maybe isn't needed because there is Ant
 import 'font-awesome/css/font-awesome.min.css';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
     <App />,
