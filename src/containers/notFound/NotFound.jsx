921d135be964ac0e874856593bde0052b1e3720a
RipDevil <tiberi66@yandex.ru>
2019-12-19 17:11:03 +0700

    Changed sublabel 404 style & interval from 404 to the root

--- src/containers/notFound/NotFound.jsx
+++ src/containers/notFound/NotFound.jsx
@@ -1,27 +1,29 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
 import { Row, Col, Typography } from "antd";
 import sorry1 from '../../images/notFound/404.jpg';
 
 // TODO: implement 3 random weresorry pics
 const NotFound = () => {
+    const [redirect, setRedirect] = useState(false);
+    setTimeout(() => setRedirect(true), 5000);
     return (
-        <Row className="centrify" type="flex" justify="center" align="middle">
-            <Col>
-                <Row type="flex" justify="center" align="middle">
-                    <Typography.Title className="header-404">404</Typography.Title>
-                </Row>
-                <Row type="flex" justify="center" align="middle">
-                    <Typography.Title ellipsis disabled>Page not found, we're sorry</Typography.Title>
-                </Row>
-                <Row type="flex" justify="center" align="middle">
-                        <img style={{
-                        height: "300px",
-                        width: "auto"
-                    }} src={sorry1} alt="And for this" />
-                </Row>
-            </Col>
-        </Row>
+        <>
+            { redirect && <Redirect to="/" /> }
+            <Row className="centrify" type="flex" justify="center" align="middle">
+                <Col>
+                    <Row type="flex" justify="center" align="middle">
+                        <Typography.Title className="header-404">404</Typography.Title>
+                    </Row>
+                    <Row type="flex" justify="center" align="middle">
+                        <Typography.Title ellipsis className="subHeader-404">Page not found, we're sorry</Typography.Title>
+                    </Row>
+                    <Row type="flex" justify="center" align="middle">
+                            <img src={sorry1} className="weresorry-404" alt="And for this" />
+                    </Row>
+                </Col>
+            </Row>
+        </>
     );
 };
 
