2af513703b627e6942f8b47bb6e7c50663380528
MarkAnn <tiberi64@yandex.ru>
2020-08-03 21:52:13 +0700

    login form rework

--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -1,50 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useStore } from "effector-react";
 import { Redirect, withRouter } from "react-router-dom";
-import { Row, Col, Typography, Form, Input, Icon, Button } from "antd";
+import { Row, Col, Typography, Form, Input, Button } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
 
 import { getRandomColor } from "utils/utils";
-import { $config } from "models/appConfig";
-
-import {
-    loginApi,
-    passwordApi,
-    nameApi,
-    lastnameApi,
-    $form,
-    setIsRegister,
-    auth } from "models/loginForm";
-
-import { userLogged } from "models/user";
+import { $config } from "stores";
 
 const Login = ({ IsLoggedIn }) => {
-    const [headerColor, setHeaderColor, GetRandomColor] = [...React.useState({ color: "#000" }), () => { getRandomColor(setHeaderColor) }];
-    const { config } = useStore($config);
-    const formStore = useStore($form);
-    const log = () => {
-        // console.log(formStore);
-        auth({
-            signIn: formStore.isRegister,
-            params: {
-                username: formStore.login.value,
-                password: formStore.password.value,
-                name: formStore.name.value,
-                lastname: formStore.lastname.value
-            }
-        })
-        .then(({ data }) => {
-            userLogged({
-                login: formStore.login.value,
-                token: data.token
-            })
-        })
-        .catch((e) => { 
-            if (e && e.data) {
-                console.error(e.data.status, e.data.message);
-            } else {
-                console.error(e);
-            }
-        })
+    const { content: links } = useStore($config);
+    const [headerColor, setHeaderColor, GetRandomColor] = [...useState({ color: "#000" }), () => getRandomColor(setHeaderColor)];
+    const [isRegister, setRegister] = useState(false);
+
+    const onSubmit = (result) => {
+        console.log('result :>> ', result);
     };
 
     return (
@@ -53,55 +22,51 @@ const Login = ({ IsLoggedIn }) => {
             <Row className="centrify" type="flex" justify="center" align="middle">
                 <Col>
                     <Row type="flex" justify="center" align="middle">
-                        <Typography.Title title="GitHub" onMouseEnter={GetRandomColor} onClick={() => window.open(config.links.external_link)} style={headerColor} className="login-header" copyable={false}>Musi<sup><strong><i className="fa fa-times" aria-hidden="true"></i></strong></sup></Typography.Title>
+                        <Typography.Title
+                            title="GitHub"
+                            onMouseEnter={GetRandomColor}
+                            onClick={() => window.open(links.external_link)}
+                            style={headerColor}
+                            className="login-header"
+                            copyable={false}
+                        >
+                            Musi<sup><strong><i className="fa fa-times" aria-hidden="true"></i></strong></sup>
+                        </Typography.Title>
                     </Row>
 
-                    {formStore.isRegister && <Row type="flex" justify="start" align="middle"><Typography.Text>Sign up</Typography.Text></Row>}
+                    {isRegister && <Row type="flex" justify="start" align="middle"><Typography.Text>Sign up</Typography.Text></Row>}
 
-                    <Form onSubmit={(e) => { e.preventDefault(); }} className="login-form">
-                        <Form.Item>
+                    <Form onFinish={onSubmit} onFinishFailed={GetRandomColor} className="login-form">
+                        <Form.Item name="login" rules={[{required: true}]}>
                             <Input
-                                value={formStore.login.value}
-                                onChange={(e) => loginApi.changed(e.target.value)}
-                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
                                 placeholder="Login"
                             />
                         </Form.Item>
-                        <Form.Item>
+                        <Form.Item name="password" rules={[{required: true}, {min: 5, max: 30, type: "string"}]}>
                             <Input
-                                className={formStore.password.error ? "input-error" : ""}
-                                value={formStore.password.value}
-                                onChange={(e) => passwordApi.changed(e.target.value)}
-                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
                                 type="password"
                                 placeholder="Password"
                             />
                         </Form.Item>
-                        {formStore.isRegister &&
+                        {isRegister &&
                             <>
-                                <Form.Item>
-                                    <Input
-                                        value={formStore.name.value}
-                                        onChange={(e) => nameApi.changed(e.target.value)}
-                                        placeholder="Name"
-                                    />
+                                <Form.Item name="name" rules={[{required: true}, {min: 1, max: 30, type: "string"}]}>
+                                    <Input placeholder="Name" />
                                 </Form.Item>
-                                <Form.Item>
-                                    <Input
-                                        value={formStore.lastname.value}
-                                        onChange={(e) => lastnameApi.changed(e.target.value)}
-                                        placeholder="Last name"
-                                    />
+                                <Form.Item name="lastname" rules={[{required: true}, {min: 1, max: 30, type: "string"}]}>
+                                    <Input placeholder="Last name" />
                                 </Form.Item>
                             </>}
                         <Form.Item>
                             <Row type="flex" justify="end">
-                                <Button onClick={() => { setIsRegister(!formStore.isRegister); }} type="link" htmlType="button">
-                                    {!formStore.isRegister ? "Sign Up" : "Sign In"}
+                                <Button onClick={() => { setRegister(!isRegister); GetRandomColor(); }} type="link" htmlType="button">
+                                    {!isRegister ? "Sign Up" : "Sign In"}
                                 </Button>
 
-                                <Button onClick={log} type={formStore.isRegister ? "danger" : "primary"} htmlType="submit" disabled={!formStore.canLog}>
-                                    {formStore.isRegister ? "Sign Up" : "Sign In"}
+                                <Button type={isRegister ? "danger" : "primary"} htmlType="submit" disabled={false}>
+                                    {isRegister ? "Sign Up" : "Sign In"}
                                 </Button>
                             </Row>
                         </Form.Item>
